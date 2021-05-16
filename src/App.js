714d38a04568174ae1774394df6ed8b55359e974
mepro500 <abunawaf5161@gmail.com>
2021-05-16 11:14:14 +0300

    updated headline

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>hello implify 
         </p>
         <a
           className="App-link"
